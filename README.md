40f6aed61440fa01eef68769ec6a7884850a149f
idakukimiya <kukimiya.ida@gmail.com>
2022-08-16 14:34:49 -0400

    cleaning up script.js

--- README.md
+++ README.md
@@ -2,41 +2,55 @@
 
 ## Description
 
+Deployed URL: ("https://idakukimiya.github.io/open-sesame/")
 
-Deployed URL: https://idakukimiya.github.io/open-sesame/
-
-GitHub URL: https://github.com/idakukimiya/open-sesame
-
+GitHub URL: ("https://github.com/idakukimiya/open-sesame")
 
 ![alt text](assets/image/screenshotOS.cbc1.png)
 ![alt text](assets/image/screenshotOS.cbc2.png)
 
 Tasks
------
-- Read requirements 3-5 times
-- List questions to ask
-- Make a list of items to research
-- Get questions answerwed
-- Do research
-- Break down app into smaller problems
-- Choose problem to start with
-- Pseudocode the problem
-- Code the problem
-- Debug the problem
-- Choose next problem and repeat steps
-- Deploy the app
-- Test the deployed app using the links i will submit
-- Create a professional readme
+
+* Read requirements 3-5 times
+
+* List questions to ask
+
+* Make a list of items to research
+
+* Get questions answerwed
+
+* Do research
+
+* Break down app into smaller problems
+
+* Choose problem to start with
+
+* Pseudocode the problem
+
+* Code the problem
+
+* Debug the problem
+
+* Choose next problem and repeat steps
+
+* Deploy the app
+
+* Test the deployed app using the links i will submit
+
+* Create a professional readme
 
 Research
---------
-- How do I check the user entered length? (Hint: use conditional)
-- How do I use Math.random() to pick a random letter?
-- How to randomly select from a specific group of characters? (e.g. upper vs lower)
-- https://w3collective.com/random-password-generator-javascript/
+
+* How do I check the user entered length? (Hint: use conditional)
+
+* How do I use Math.random() to pick a random letter?
+
+* How to randomly select from a specific group of characters? (e.g. upper vs lower)
+
+* ("https://w3collective.com/random-password-generator-javascript/")
 
 Steps
------
+
 DECLARE VARS
 string password (the result)
 num passLength (user input)
@@ -52,7 +66,7 @@ arr choices (array containing all characters chosen by the user)
 
 PROMPT user for password length
 
-CONFIRM if user 
+CONFIRM if user
 would like lower case
 IF user would like lower case
 THEN add all lowerChars to choices
@@ -62,5 +76,3 @@ CONFIRM if user would like upper case
 CONFIRM if user would like special characters
 
 CONFIRM if user would like numbers
-
-
--- assets/script.js
+++ assets/script.js
@@ -1,78 +1,92 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-
-// Created Arrays of Possible Character Choices
-var numbers = ("0123456789");
-var alpha = ("abcdefghijklmnpoqrstuywxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-var special = ("!@#$%^&*_-+=");
-
-// Created function to ask user which options and made conditional statements to ensure requirements were met.
-function questions() {
-  var isValid = false;
-  do {
-    var length = prompt("Password need to be between 8 and 128 characters");
-    var askNumbers = confirm("Do your password include numbers?");
-    var askAlpha = confirm("Do your password to contain both lower and upper letter?");
-    var askSpecial = confirm("Password need to include special characters?");
-    var responses = {
-      length: length,
-      askNumbers: askNumbers,
-      askAlpha: askAlpha,
-      askSpecial: askSpecial
-    } 
-    if((length < 8)||(length > 128))
-    alert("Choose number between 8 and 128");
-    else if((!askNumbers)&&(!askLowerCase)&&(!askUpperCase)&&(!askSpecial))
-    alert("Must choose at least one type.");
-    else
-    isValid = true;
-
-  } while(!isValid);
-  return responses;
-}
-// This function joins all the user responses and then creates the result - a strong password.
+// Assignment code here
 function generatePassword() {
-  var passwordOptions = questions();
-  var possibleCombo = [];
-  var finalPassword = "";
+  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+  var possibleCharacters = [];
 
-
-
-  if (passwordOptions.askNumbers) {
-    for (var i of numbers)
-      possibleCombo.push(i);
+  // get input and validate
+  numberOfCharacters = prompt("How many characters do you want in your password? Choose between 8-128 characters.");
+  if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+    return "Please choose a valid number of characters.";
+  } else if (isNaN(numberOfCharacters)) {
+    numberOfCharacters = prompt("Please enter a valid number.");
   }
-  if (passwordOptions.askAlpha) {
-    for (var i of alpha)
-      possibleCombo.push(i);
-  }
-
-  if (passwordOptions.askSpecial) {
-    for (var i of special)
-      possibleCombo.push(i);
-  }
-
-
-  console.log(possibleCombo);
-
-
-  for (var i = 0; i < passwordOptions.length; i++) {
-    finalPassword += possibleCombo[Math.floor(Math.random() * possibleCombo.length)];
+  else {
+    alert("Your password will be " + numberOfCharacters + " characters long.");
     
   }
-  console.log(finalPassword);
 
+  hasLowercase = confirm("Do you want lowercase characters?");
+  if (hasLowercase) {
+    var turnToLowercase = alert("Your password will have lowercase characters.");
+  }
+  else {
+    alert("Your password will NOT have lowercase characters.");
+  }
+
+  hasUppercase = confirm("Do you want uppercase characters?");
+  if (hasUppercase) {
+    alert("Your password will have uppercase characters.");
+  }
+  else {
+    alert("Your password will NOT have uppercase characters.");
+  }
+
+  hasNumbers = confirm("Do you want to use numbers?");
+  if (hasNumbers) {
+    alert("Your password will have numbers.");
+  }
+  else {
+    alert("Your password will NOT have numbers.");
+  }
+
+  hasSpecial = confirm("Do you want special characters?");
+  if (hasSpecial) {
+    alert("Your password will have special characters.");
+  }
+  else {
+    alert("Your password will NOT have special characters.");
+  }
+
+  if (hasLowercase === false && hasUppercase === false && hasNumbers === false && hasSpecial === false) {
+    return "Please select at least one character type.";
+  };
+
+  // group selected characters
+  if (hasLowercase) {
+    possibleCharacters = possibleCharacters.concat(lowercaseCharacters);
+  }
+  if (hasUppercase) {
+    possibleCharacters = possibleCharacters.concat(uppercaseCharacters);
+  }
+  if (hasNumbers) {
+    possibleCharacters = possibleCharacters.concat(numericCharacters);
+  }
+  if (hasSpecial) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+  }
+
+  // pick random cards out of new pool for length of password
+  let finalPassword = ""
+  for (let i = 0; i < numberOfCharacters; i++) {
+    let rng =[Math.floor(Math.random() * possibleCharacters.length)];
+    // or finalPassword += possibleCharacters[rng];
+    finalPassword = finalPassword + possibleCharacters[rng];
+  }
   return finalPassword;
-}
+};
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
