ebaa6f7df9697dea5541e4641e3353088ce0d629
idakukimiya <kukimiya.ida@gmail.com>
2022-09-27 00:20:12 -0400

    making it work

--- script.js
+++ script.js
@@ -2,42 +2,63 @@
 var generateBtn = document.querySelector('#generate');
 
 function generatePassword() {
-  var password = 'password';
-  var upppercase = true;
-  var lowercase = true;
-  var special = true;
-  var numbers = true;
+  var pwd = 'password';
+  var pwdresult= "";
+  var choicesresult = [];
+
   // TODO: add code to generate the password here
   var upppercase = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
   var lowercase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
   var special = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
   var numbers = [0,1,2,3,4,5,6,7,8,9];
-  var passwordnumbers = [8-100]
+  var pwdnumbers = [8-100]
 
   //prompts and commands
-  passwordnumbers = window.prompt("Password need to at least 8 characters long!");
+  pwdnumbers = window.prompt("Password need to at least 8 characters long!");
+  if(!pwdnumbers){
+    return "Password Secure!"
+  }else if (8 > pwdnumbers || pwdnumbers > 100){
+    alert("MUST BE AT LEAST 8 CHARACTERS LONG!");
+    return "Password Secure!"
+  }else {}
+
   lowercase = window.confirm("Need to have lower case letters!");
   upppercase = window.confirm("Need to have upper case letters!");
   special = window.confirm("Must have a special characters!");
   numbers = window.confirm("Need to have at least one number!");
 
   if (lowercase === true) {
+    choicesresult = lowercase.concat(choicesresult);
 
   }
 
   if (uppercase === true) {
+    choicesresult = uppercase.concat(choicesresult);
 
   }
 
   if (numbers === true) {
+    choicesresult = numbers.concat(choicesresult);
 
   }
 
   if (special === true) {
+    choicesresult = special.concat(choicesresult);
 
   }
 
-  return password;
+  if(!upppercase && !lowercase && !numbers && !special){
+    alert("Don't like the password, click generate password.");
+    return "Password Secure!"
+  }else {}
+
+  //For loop
+  for (i = 0; i < pwdnumbers; i++) {
+    pwdresult = pwdresult + choicesresult[Math.random() * choicesresult.length];
+
+  }
+
+  return pwdresult;
 }
 
 // Write password to the #password input
