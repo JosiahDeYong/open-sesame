ca49a59a234888a8631e09e608edd1a1ec41f6f1
idakukimiya <kukimiya.ida@gmail.com>
2022-09-26 23:47:17 -0400

    change syntax in script.js

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ var generateBtn = document.querySelector('#generate');
 function generatePassword() {
   var password = 'password';
   // TODO: add code to generate the password here
+  var upppercase = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
+  var lowercase = ["q", "w", "e", "r",];
+  var special = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+  var numbers = [0,1,2,3,4,5,6,7,8,9];
+  var passwordnumbers = [8-100]
+
+  //prompts and commands
+  passwordnumbers = window.prompt("Password need to at least 8 characters long!");
+  lowercase = window.confirm("Need to have lower case letters!");
+  upppercase = window.confirm("Need to have upper case letters!");
+  special = window.confirm("Must have a special characters!");
+  numbers = window.confirm("Need to have at least one number!")
 
   return password;
 }
