abce05a57bdf9fa83f7d3e40a7aa78fb41ee7b83
JosiahDeYong <112961787+JosiahDeYong@users.noreply.github.com>
2024-12-10 10:57:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,63 @@
-// Assignment Code
+// Get slider and display elements
 var generateBtn = document.querySelector('#generate');
+var lengthSlider = document.querySelector('#passwordLength');
+var lengthDisplay = document.querySelector('#lengthValue');
+
+// Update the display value when the slider changes
+lengthSlider.addEventListener('input', function () {
+  lengthDisplay.textContent = lengthSlider.value;
+});
 
 function generatePassword() {
   var pwdresult = "";
-  var pwdlength;
-  var userchoices = [];
-  // TODO: add code to generate the password here
+  var guaranteedChars = [];
+
   var uppercase = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
   var lowercase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
   var special = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
-  var numbers = [0,1,2,3,4,5,6,7,8,9];
+  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  //prompts and commands
-  pwdlength = prompt("Password must be at least 8 characters long!");
+  // Get password length from the slider
+  var pwdlength = parseInt(lengthSlider.value);
 
-  if(!pwdlength){
-    return "Password Secure!"
-  } else if (8 > pwdlength || pwdlength > 100)  {
-    alert("MUST BE AT LEAST 8 CHARACTERS LONG!");
-    return "Password Secure!"
-  } else {}
+  var confirmupper = document.querySelector('#includeCapitals').checked;
+  var confirmnumbers = document.querySelector('#includeNumbers').checked;
+  var confirmspecial = document.querySelector('#includeSpecials').checked;
 
-  var confirmlower = confirm("Need to have lower case letters!");
-  var confirmupper = confirm("Need to have upper case letters!");
-  var confirmspecial = confirm("Must have a special characters!");
-  var confirmnumbers = confirm("Need to have at least one number!");
+  var pickList = [lowercase];
 
-  if (confirmlower === true) {
-    userchoices = lowercase.concat(userchoices);
-
+  if (confirmupper) {
+    pickList.push(uppercase);
+    pwdresult += uppercase[Math.floor(Math.random() * uppercase.length)];
+  }
+  if (confirmnumbers) {
+    pickList.push(numbers);
+    pwdresult += numbers[Math.floor(Math.random() * numbers.length)];
+  }
+  if (confirmspecial) {
+    pickList.push(special);
+    pwdresult += special[Math.floor(Math.random() * special.length)];
   }
 
-  if (confirmupper === true) {
-    userchoices = uppercase.concat(uppercase);
+  pwdresult = pwdresult.concat(guaranteedChars);
 
+  while (pwdresult.length < pwdlength) {
+    var conga = pickList[Math.floor(Math.random() * pickList.length)];
+    pwdresult += conga[Math.floor(Math.random() * conga.length)];
   }
 
-  if (confirmnumbers === true) {
-    userchoices = numbers.concat(userchoices);
-
-  }
-
-  if (confirmspecial === true) {
-    userchoices = special.concat(userchoices);
-
-  }
-
-  if(!confirmupper && !confirmlower && !confirmnumbers && !confirmspecial){
-    alert("Don't like the password, click generate password.");
-    return "Password Secure!"
-  }else {}
-
-  //For loop
-  for (i = 0; i < pwdlength; i++) {
-    pwdresult = pwdresult + userchoices[Math.floor(Math.random() * userchoices.length)];
-  }
+  pwdresult = shuffleString(pwdresult);
 
   return pwdresult;
+}
+
+function shuffleString(str) {
+  let arr = str.split('');
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr.join('');
 }
 
 // Write password to the #password input
